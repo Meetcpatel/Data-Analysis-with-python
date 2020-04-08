43030a6598a41dfb0c8ec49ce14dfcf089c435f7
Meet Patel <meetcpatel906@gmail.com>
2020-04-08 12:40:48 +0530

    Importing dataset into R

---
+++ ML_Linear_regression_model_in_R/excersise/data_processing.r
@@ -0,0 +1,2 @@
+df <- read.csv('D:/Data-Analysis-with-python/ML_Linear_regression_model_in_R/excersise/project_excersise1.csv', header= TRUE)
+str(df)
--- data.py
+++ data.py
@@ -1,27 +1,10 @@
-# Import pandas library
-import pandas as pd
 
-# Read the online file by the URL provides above, and assign it to variable "df"
-other_path = "https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DA0101EN/auto.csv"
-df = pd.read_csv(other_path, header = None)
+import os, uuid
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-# show the first 5 rows using dataframe.head() method
-print("The first 5 rows of the dataframe\n") 
+print("\nListing blobs...")
 
-headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
-         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
-         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
-         "peak-rpm","city-mpg","highway-mpg","price"]
-
-#print("headers\n", headers)
-
-df.columns=headers
-
-pricedrop = df.dropna(subset=["price"], axis=0)
-
-topfive = df.head(100)
-
-print(topfive)
-
-df.to_csv("automobile.csv", index=False)
-
+# List the blobs in the container
+blob_list = container_client.list_blobs()
+for blob in blob_list:
+    print("\t" + blob.name)
